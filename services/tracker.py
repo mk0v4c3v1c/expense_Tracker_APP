8b49e2ed2a0a57f37b7127b8f259f10b2fe724f6
mk0v4c3v1c <mk0v4c3v1c@gmail.com>
2025-02-06 10:15:08 +0100

    main-update

--- services/tracker.py
+++ services/tracker.py
@@ -5,22 +5,33 @@ from models.category import Category
 class ExpenseTracker:
     def __init__(self):
         self.transactions = DataHandler.load_transactions()
-
+    #Delete transaction per index
+    def delete_transaction(self, index: int):
+        if 0 <= index < len(self.transactions):
+            deleted_transaction = self.transactions.pop(index)
+            DataHandler.save_transactions(self.transactions)
+            print(f"Transaction deleted: {deleted_transaction.to_dict()}")
+        else:
+            print("Invalid transaction index. Please try again.")
     #Add new transaction if it's valid
     def add_transaction(self, amount: float, category: str, description: str = ""):
         if not Category.is_valid(category):
             raise ValueError("Not a valid category!")
-
+        #Transactions per description
         transaction = Transaction(amount, category, description=description)
         self.transactions.append(transaction)
         DataHandler.save_transactions(self.transactions)
         print(f"Added Transaction: {transaction.to_dict()}")
-
-    #Show all transactions
+    #List transactions
     def list_transactions(self):
+        if not self.transactions:
+            print(" No transactions recorded yet.")
+            return
+
+        print("\n Transaction List:")
         for t in self.transactions:
             print(f"{t.date} | {t.category} | {t.amount} RSD | {t.description}")
 
-    #Calculate current balance
     def calculate_balance(self):
         return sum(t.amount for t in self.transactions)
+
