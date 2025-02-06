52968ba1f5406c695c3d0d42d03e478e324ff0e8
mk0v4c3v1c <mk0v4c3v1c@gmail.com>
2025-02-06 09:54:32 +0100

    main

---
+++ main.py
@@ -0,0 +1,25 @@
+from services.tracker import ExpenseTracker
+
+def main():
+    tracker = ExpenseTracker()
+
+    while True:
+        print("\n1. Add transaction\n2. Show transactions\n3. Show bilance\n4. Exit")
+        choice = input("Choose an option: ")
+
+        if choice == "1":
+            amount = float(input("Enter the amount: "))
+            category = input("Enter the category: ")
+            description = input("Description (optional): ")
+            tracker.add_transaction(amount, category, description)
+        elif choice == "2":
+            tracker.list_transactions()
+        elif choice == "3":
+            print(f"Trenutni bilans: {tracker.calculate_balance()} RSD")
+        elif choice == "4":
+            break
+        else:
+            print("Unknown choice!")
+
+if __name__ == "__main__":
+    main()
