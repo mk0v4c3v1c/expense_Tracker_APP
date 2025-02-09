a0778835fbe5686ae151a7d1f155630d4934962f
mk0v4c3v1c <mk0v4c3v1c@gmail.com>
2025-02-09 19:53:36 +0100

    main-update

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def main():
     tracker = ExpenseTracker()
 
     while True:
-        print("\n1. Add transaction\n2. Show transactions\n3. Show bilance\n4. Exit")
+        print("\n1. Add transaction\n2. Show transactions\n3. Show bilance\n4. Delete transaction\n5. Exit")
         choice = input("Choose an option: ")
 
         if choice == "1":
@@ -17,6 +17,10 @@ def main():
         elif choice == "3":
             print(f"Trenutni bilans: {tracker.calculate_balance()} RSD")
         elif choice == "4":
+            tracker.list_transactions()
+            index = int(input("Enter the transaction index for deletion : "))
+            tracker.delete_transaction(index)
+        elif choice == "5":
             break
         else:
             print("Unknown choice!")
